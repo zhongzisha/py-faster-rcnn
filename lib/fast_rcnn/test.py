2434edbc09bbda42537d8902160ffa1f8a119534
zhongzisha <zhongzisha@outlook.com>
2016-04-11 17:54:03 +0800

    add print output in test.py

--- lib/fast_rcnn/test.py
+++ lib/fast_rcnn/test.py
@@ -327,7 +327,8 @@ def test_net(net, imdb, max_per_image=100, thresh=0.05, vis=False):
         
         if seg is not None:
             seg_filepath = os.path.join(output_dir, os.path.basename(im_filepath)[:-4], '.png')
-            print seg_filepath + ' done!' + seg.shape
+            print seg.shape
+            print seg_filepath + ' done!'
             scipy.misc.toimage(seg, cmin=0, cmax=255).save(seg_filepath) 
 
         _t['misc'].tic()
