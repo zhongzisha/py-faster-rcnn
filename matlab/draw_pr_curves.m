615e67c0ffcf9f6943cdcc62ba52c289d88abd77
zhongzisha <zhongzisha@outlook.com>
2016-09-22 08:36:54 +0800

    add models/vaihingen/d_ZF2

--- matlab/draw_pr_curves.m
+++ matlab/draw_pr_curves.m
@@ -1,18 +1,13 @@
 
-nets = {'zf','vgg16','d_zf','d_vgg16','d_zf1','d_vgg161'};
-colors = 'rgby';
+nets = {'zf','vgg16','d_zf','d_vgg16','d_zf1','d_vgg161','d_vgg162'};
+colors = VOClabelcolormap(20); %rand(20,3);
 legend_strs = cell(1, length(nets));
 
-VOCinit;
-ind = 1;
-for i=1:2:length(nets)
+VOCinit; 
+for i=1:length(nets)
 tic,[rec,prec,ap] = VOCevaldet(VOCopts,nets{i},'car',0);
 legend_strs{i} = sprintf('%s(%.4f)',nets{i},ap);
-plot(rec,prec,'-','Color',colors(ind));hold on
-tic,[rec,prec,ap] = VOCevaldet(VOCopts,nets{i+1},'car',0);
-legend_strs{i+1} = sprintf('%s(%.4f)',nets{i+1},ap);
-plot(rec,prec,'-.','Color',colors(ind));hold on
-ind = ind + 1;
+plot(rec,prec,'-','Color',colors(i,:));hold on
 end
 
 grid;
