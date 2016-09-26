9f41abdbcff37b395af43ab6faadeb5f444b8ca4
zhongzisha <zhongzisha@outlook.com>
2016-09-26 14:55:17 +0800

    modified rgb into bgr

--- tools/test_net_isprs.py
+++ tools/test_net_isprs.py
@@ -6,7 +6,7 @@ python ./tools/reval.py --imdb voc_2014_val --matlab --comp \
     
     
 Usage:
-python tools/test_net_rgbd.py --gpu 2 \
+python tools/test_net_bgrd.py --gpu 2 \
 --prototxt models/vaihingen/VGG16/faster_rcnn_end2end/test.prototxt \
 --caffemodel output/faster_rcnn_end2end/voc_2013_train/vgg16_faster_rcnn_iter_70000.caffemodel \
 --devkit_path /home/nlpr2/data/VOCdevkit2013 \
@@ -16,7 +16,7 @@ python tools/test_net_rgbd.py --gpu 2 \
 --nms_threshold 0.3 \
 --set PIXEL_MEANS '[[[84,86,117]]]'
 
-python tools/test_net_rgbd.py --gpu 3 \
+python tools/test_net_bgrd.py --gpu 3 \
 --prototxt models/potsdam_d/VGG16/faster_rcnn_end2end/test.prototxt \
 --caffemodel output/faster_rcnn_end2end/voc_2014_train/d_vgg16_faster_rcnn_iter_70000.caffemodel \
 --devkit_path /home/nlpr2/data/VOCdevkit2014_for_test \
@@ -54,7 +54,7 @@ def parse_args():
                         default=None, type=str)
     parser.add_argument('--step_size', dest='step_size', help='step_size',
                         default=250, type=int)
-    parser.add_argument('--rgb_mean', dest='rgb_mean', help='rgb_mean',
+    parser.add_argument('--bgr_mean', dest='bgr_mean', help='bgr_mean',
                         default=None, type=str)
     parser.add_argument('--dsm_mean', dest='dsm_mean', help='dsm_mean',
                         default=None, type=str)
@@ -69,11 +69,11 @@ def parse_args():
 
     return args
 
-def get_seg_result(net, im, rgb_mean, dsm=None, dsm_mean=None, prob_blob_name='prob'):
+def get_seg_result(net, im, bgr_mean, dsm=None, dsm_mean=None, prob_blob_name='prob'):
     blobs = {'data' : None, 'dsm' : None}
     data_blob = np.zeros((1, im.shape[0], im.shape[1], im.shape[2]), 
                             dtype=np.float32)
-    data_blob[0, :, :, :] = im - rgb_mean  
+    data_blob[0, :, :, :] = im - bgr_mean  
     channel_swap = (0, 3, 1, 2)
     blobs['data'] = data_blob.transpose(channel_swap) 
     net.blobs['data'].reshape(*(blobs['data'].shape))
@@ -91,15 +91,15 @@ def get_seg_result(net, im, rgb_mean, dsm=None, dsm_mean=None, prob_blob_name='p
     seg_prob = net.blobs[prob_blob_name].data
     return seg_prob
 
-def test_on_one_image(net, rgb0, rgb_mean, dsm0=None, dsm_mean=None, step_size=250, num_seg_classes=6, prob_blob_name='prob'):
+def test_on_one_image(net, bgr0, bgr_mean, dsm0=None, dsm_mean=None, step_size=250, num_seg_classes=6, prob_blob_name='prob'):
     BLOCK_SIZE = 500  
         
-    height, width, num_bands = rgb0.shape
+    height, width, num_bands = bgr0.shape
     if height % BLOCK_SIZE:
         height_padsize = BLOCK_SIZE - height % BLOCK_SIZE
     if width % BLOCK_SIZE:
         width_padsize = BLOCK_SIZE - width % BLOCK_SIZE
-    rgb0 = np.pad(rgb0, ((0, height_padsize),(0, width_padsize),(0, 0)), 'reflect')
+    bgr0 = np.pad(bgr0, ((0, height_padsize),(0, width_padsize),(0, 0)), 'reflect')
     if dsm0 is not None:
         if len(dsm0.shape) == 2:
             dsm0 = np.pad(dsm0, ((0, height_padsize),(0, width_padsize)), 'reflect')
@@ -108,15 +108,15 @@ def test_on_one_image(net, rgb0, rgb_mean, dsm0=None, dsm_mean=None, step_size=2
     
     dsm = None
     seg_result = None
-    seg_result = np.zeros((num_seg_classes, rgb0.shape[0], rgb0.shape[1]),dtype=np.float32)
-    for y in xrange(0, rgb0.shape[0], step_size):
-        for x in xrange(0, rgb0.shape[1], step_size):
+    seg_result = np.zeros((num_seg_classes, bgr0.shape[0], bgr0.shape[1]),dtype=np.float32)
+    for y in xrange(0, bgr0.shape[0], step_size):
+        for x in xrange(0, bgr0.shape[1], step_size):
             # yield the current window
-            im = rgb0[y:y+BLOCK_SIZE, x:x+BLOCK_SIZE, :]
+            im = bgr0[y:y+BLOCK_SIZE, x:x+BLOCK_SIZE, :]
             if dsm0 is not None:
                 dsm = dsm0[y:y+BLOCK_SIZE, x:x+BLOCK_SIZE]
             # begin detection in a 500x500 image
-            seg_ = get_seg_result(net, im, rgb_mean, dsm, dsm_mean, prob_blob_name)
+            seg_ = get_seg_result(net, im, bgr_mean, dsm, dsm_mean, prob_blob_name)
             
             if seg_ is not None:
                 seg_result[:, y:y+BLOCK_SIZE, x:x+BLOCK_SIZE] = seg_ 
@@ -130,7 +130,7 @@ if __name__ == '__main__':
     caffe.set_mode_gpu()
     caffe.set_device(args.gpu_id)
     
-    rgb_mean = literal_eval(args.rgb_mean)
+    bgr_mean = literal_eval(args.bgr_mean)
     dsm_mean = None
     if args.dsm_mean is not None:
         dsm_mean = literal_eval(args.dsm_mean)
@@ -150,13 +150,13 @@ if __name__ == '__main__':
     
     for i in xrange(num_images):
         index = image_index[i]
-        rgb_image_path = os.path.join(data_path, 'JPEGImages', index + '.jpg')
+        bgr_image_path = os.path.join(data_path, 'JPEGImages', index + '.jpg')
         dsm_image_path = os.path.join(data_path, 'JPEGImages', index + '_depth.jpg')
-        rgb0 = cv2.imread(rgb_image_path)
+        bgr0 = cv2.imread(bgr_image_path)
         dsm0 = None
         if dsm_mean != None:  
             dsm0 = cv2.imread(dsm_image_path, cv2.IMREAD_GRAYSCALE) 
-        seg_result = test_on_one_image(net, rgb0, rgb_mean, dsm0, dsm_mean, args.step_size, prob_blob_name=args.prob_blob_name)
+        seg_result = test_on_one_image(net, bgr0, bgr_mean, dsm0, dsm_mean, args.step_size, prob_blob_name=args.prob_blob_name)
         
         if seg_result is not None:
             filename = args.save_prefix + '_det_' + args.image_set + '_' + index + '.mat'
